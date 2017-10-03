31cf5bab9e7059f9acf0475666ce22914df5f863
elteKrisztianKereszti <krisztian.kereszti@gmail.com>
2017-10-04 00:13:51 +0200

    first commit

---
+++ src/main/java/hu/elte/alkfejl/issuetracker/controller/UserController.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hu.elte.alkfejl.issuetracker.controller;
+
+import hu.elte.alkfejl.issuetracker.model.User;
+//import hu.elte.alkfejl.issuetracker.service.UserService;
+//import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.*;
+
+/**
+ *
+ * @author kkereszti
+ */
+
+@Controller
+@RequestMapping("/user")
+public class UserController {
+    
+    @GetMapping("/greet")
+    public String greeting(@RequestParam(value = "name", required = false, defaultValue = "World") String name, Model model) {
+        model.addAttribute("name", name);
+        return "greeting";
+    }
+}
